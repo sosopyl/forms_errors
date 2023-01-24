c21e28a99b944ba904cd10b4832ea1375e251799
sosopyl <122552117+sosopyl@users.noreply.github.com>
2023-01-24 09:42:20 +0300

    Create forms_errors.js

---
+++ forms_errors.js
@@ -0,0 +1,34 @@
+// Поиск ошибок в коде с формами в JavaScript
+
+// 1
+// let textarea = document.querySelector('textarea');
+// let div = document.querySelector('div');
+
+// textarea.addEventListener('blur', function() {
+// 	div.textContent = textarea.value;
+// }); 
+
+// 2
+// let checkbox = document.querySelector('input');
+// let btn = document.querySelector('button');
+// let div = document.querySelector('div');
+
+// btn.addEventListener('click', function() {
+// 	if (checkbox.checked == true ) {
+// 		div.textContent = '111';
+// 	} else {
+// 		div.textContent = '222';
+// 	}
+// });
+
+// 3
+// let checkbox = document.querySelector('[type="checkbox"]');
+// let button = document.querySelector('button');
+
+// button.addEventListener('click', function() {
+// 	if (checkbox.checked == true) {
+// 		console.log('+++');
+// 	} else {
+// 		console.log('---');
+// 	}
+// });
